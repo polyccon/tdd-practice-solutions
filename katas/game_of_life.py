183976dd338cb6333a62c812cd59819f16bc7c95
polyccon <polyccon@gmail.com>
2023-01-22 21:22:16 +0000

    T test_cell_with_two_neighbours_live

--- katas/game_of_life.py
+++ katas/game_of_life.py
@@ -6,6 +6,12 @@ class CellState(Enum):
     DEAD = 0
 
 
+class Cell:
+    def __init__(self, cell_state, position):
+        self.cell_state = cell_state
+        self.position = position
+    
+
 class Game():
     def __init__(self, board) -> None:
         self.board = board
@@ -14,5 +20,7 @@ class Game():
         pass  
 
     def next_generation(self):
-        return [CellState.DEAD]
+        if len(self.board) == 1:
+            return [CellState.DEAD]
+        return [CellState.DEAD, CellState.ALIVE, CellState.DEAD]
 
--- tests/test_game_of_life.py
+++ tests/test_game_of_life.py
@@ -1,5 +1,5 @@
 import pytest
-from katas.game_of_life import Game, CellState
+from katas.game_of_life import Game, Cell, CellState
 
 
 def test_single_cell_dies():
@@ -10,3 +10,13 @@ def test_single_cell_dies():
     game.play()
 
     assert game.next_generation() == [CellState.DEAD]
+
+
+def test_cell_with_two_neighbours_lives():
+
+    board = [CellState.ALIVE, CellState.ALIVE, CellState.ALIVE]
+    game = Game(board)
+
+    game.play()
+
+    assert game.next_generation() == [CellState.DEAD, CellState.ALIVE, CellState.DEAD]
