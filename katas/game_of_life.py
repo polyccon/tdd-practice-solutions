8b0cf424ca8e7445c4291e355b1bde97f9daebdc
polyccon <polyccon@gmail.com>
2023-02-15 20:16:32 +0000

    refactor: code now works counting alive neighbours

--- katas/game_of_life.py
+++ katas/game_of_life.py
@@ -10,12 +10,35 @@ class Position:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        
+
     def __repr__(self):
         return str(self.x) + "-" + str(self.y)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+
+    def _is_neighbour(self, other):
+        return self.x - other.x in [ -1, 1] or self.y - other.y in [ -1, 1]
+
+
+class Board:
+    def __init__(self, cells):
+        self.cells = cells
+
+    def get_alive_neighbours_count(self, cell):
+        alive_neighbours = 0
+        for _cell in self.cells:
+            if cell.is_neighbour(_cell):
+                alive_neighbours += 1
+        return alive_neighbours
+
+    def evolve_cell_state(self):
+        evolved_cells = []
+        for cell in cells:
+            alive_neighbours = self.get_alive_neighbours_count(cell)
+            cell.evolve_cell_state(alive_neighbours)
+            evolved_cells.append(cell)
+        return evolved_cells
 
 
 class Cell:
@@ -29,17 +52,26 @@ class Cell:
            and self.position == other.position
             )
 
+    def is_neighbour(self, other):
+        return self.position._is_neighbour(other.position)
+
+    def evolve_cell_state(self, alive_neighbours_count):
+        if alive_neighbours_count > 1:
+            self.cell_state = CellState.ALIVE
+            return
+        self.cell_state = CellState.DEAD
+
 
 class Game():
     def __init__(self, board) -> None:
-        self.board = board
+        self.board = Board(board)
+
 
     def play(self):
-        pass  
+        for cell in self.board.cells:
+            alive_neighbours_count = self.board.get_alive_neighbours_count(cell)
+            cell.evolve_cell_state(alive_neighbours_count)
+        return self.board.cells
 
     def next_generation(self):
-        output = []
-        for index, cell in enumerate(self.board):
-            cell_state = CellState.DEAD if index in [0, len(self.board)-1] else CellState.ALIVE
-            output.append(Cell(cell_state, Position(cell.position.x, cell.position.y)))
-        return output
+        return self.play()
--- tests/test_game_of_life.py
+++ tests/test_game_of_life.py
@@ -4,67 +4,67 @@ from katas.game_of_life import Game, Cell, CellState, Position
 
 def test_single_cell_dies():
 
-    board = [Cell(CellState.ALIVE, Position(0,0))]
-    expected_output = [Cell(CellState.DEAD, Position(0,0))]
+    board = [Cell(CellState.ALIVE, Position(0, 0))]
+    expected_output = [Cell(CellState.DEAD, Position(0, 0))]
     game = Game(board)
 
-    game.play()
+    output = game.next_generation()
 
-    assert game.next_generation() == expected_output
+    assert output == expected_output
 
 
 def test_two_cells_die():
 
     board = [
-        Cell(CellState.ALIVE, Position(0, 0)), 
-        Cell(CellState.ALIVE, Position(0,1))
+        Cell(CellState.ALIVE, Position(0, 0)),
+        Cell(CellState.ALIVE, Position(0, 1))
     ]
     expected_output = [
-        Cell(CellState.DEAD, Position(0, 0)), 
-        Cell(CellState.DEAD, Position(0,1))
+        Cell(CellState.DEAD, Position(0, 0)),
+        Cell(CellState.DEAD, Position(0, 1))
     ]
     game = Game(board)
 
-    game.play()
+    output = game.next_generation()
 
-    assert game.next_generation() == expected_output
+    assert output == expected_output
 
 
 def test_cell_with_two_neighbours_lives():
 
     board = [
-        Cell(CellState.ALIVE, Position(0, 0)), 
-        Cell(CellState.ALIVE, Position(0,1)), 
-        Cell(CellState.ALIVE, Position(0,2))
+        Cell(CellState.ALIVE, Position(0, 0)),
+        Cell(CellState.ALIVE, Position(0, 1)),
+        Cell(CellState.ALIVE, Position(0, 2))
     ]
     expected_output = [
-        Cell(CellState.DEAD, Position(0, 0)), 
-        Cell(CellState.ALIVE, Position(0,1)), 
-        Cell(CellState.DEAD, Position(0,2))
+        Cell(CellState.DEAD, Position(0, 0)),
+        Cell(CellState.ALIVE, Position(0, 1)),
+        Cell(CellState.DEAD, Position(0, 2))
     ]
     game = Game(board)
 
-    game.play()
+    output = game.next_generation()
 
-    assert game.next_generation() == expected_output
+    assert output == expected_output
 
 
 def test_four_cells_in_a_row_middle_ones_live():
 
     board = [
-        Cell(CellState.ALIVE, Position(0, 0)), 
-        Cell(CellState.ALIVE, Position(0, 1)), 
-        Cell(CellState.ALIVE, Position(0, 2)), 
+        Cell(CellState.ALIVE, Position(0, 0)),
+        Cell(CellState.ALIVE, Position(0, 1)),
+        Cell(CellState.ALIVE, Position(0, 2)),
         Cell(CellState.ALIVE, Position(0, 3))
     ]
     expected_output = [
-        Cell(CellState.DEAD, Position(0, 0)), 
-        Cell(CellState.ALIVE, Position(0, 1)), 
-        Cell(CellState.ALIVE, Position(0, 2)), 
+        Cell(CellState.DEAD, Position(0, 0)),
+        Cell(CellState.ALIVE, Position(0, 1)),
+        Cell(CellState.ALIVE, Position(0, 2)),
         Cell(CellState.DEAD, Position(0, 3))
     ]
     game = Game(board)
 
-    game.play()
+    output = game.next_generation()
 
-    assert game.next_generation() == expected_output
+    assert output == expected_output
