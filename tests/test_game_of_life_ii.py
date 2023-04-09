86c6112441f88462913be012d5c96bb15cd4b604
polyccon <polyccon@gmail.com>
2023-04-09 21:20:36 +0100

    R position two dimensional

--- tests/test_game_of_life_ii.py
+++ tests/test_game_of_life_ii.py
@@ -3,54 +3,57 @@ from katas.game_of_life_ii import Game, Cell, CellState
 
 
 def test_single_cell_dies():
-    board = [Cell(CellState.ALIVE, 0)]
+    board = [Cell(CellState.ALIVE, (0, 0))]
     game = Game(board)
 
     next_generation = game.play()
 
-    assert next_generation == [Cell(CellState.DEAD, 0)]
+    assert next_generation == [Cell(CellState.DEAD, (0, 0))]
 
 
-def test_cell_with_one_neighbour_dies():
-    board = [Cell(CellState.ALIVE, 0), Cell(CellState.ALIVE, 1)]
+def test_cell_with_one_neighbour_in_a_row_dies():
+    board = [Cell(CellState.ALIVE, (0, 0)), Cell(CellState.ALIVE, (1, 0))]
     game = Game(board)
 
     next_generation = game.play()
 
-    assert next_generation == [Cell(CellState.DEAD, 0), Cell(CellState.DEAD, 1)]
+    assert next_generation == [
+        Cell(CellState.DEAD, (0, 0)),
+        Cell(CellState.DEAD, (1, 0)),
+    ]
 
 
-def test_cell_with_two_neighbours_lives():
+def test_cell_with_two_neighbours_in_a_row_lives():
     board = [
-        Cell(CellState.ALIVE, 0),
-        Cell(CellState.ALIVE, 1),
-        Cell(CellState.ALIVE, 2),
+        Cell(CellState.ALIVE, (0, 0)),
+        Cell(CellState.ALIVE, (1, 0)),
+        Cell(CellState.ALIVE, (2, 0)),
     ]
     game = Game(board)
 
     next_generation = game.play()
 
     assert next_generation == [
-        Cell(CellState.DEAD, 0),
-        Cell(CellState.ALIVE, 1),
-        Cell(CellState.DEAD, 2),
+        Cell(CellState.DEAD, (0, 0)),
+        Cell(CellState.ALIVE, (1, 0)),
+        Cell(CellState.DEAD, (2, 0)),
     ]
 
 
-def test_cell_with_three_neighbours_lives():
+def test_cell_with_three_neighbours_in_a_row_lives():
     board = [
-        Cell(CellState.ALIVE, 0),
-        Cell(CellState.ALIVE, 1),
-        Cell(CellState.ALIVE, 2),
-        Cell(CellState.ALIVE, 3),
+        Cell(CellState.ALIVE, (0, 0)),
+        Cell(CellState.ALIVE, (1, 0)),
+        Cell(CellState.ALIVE, (2, 0)),
+        Cell(CellState.ALIVE, (3, 0)),
     ]
     game = Game(board)
 
     next_generation = game.play()
 
     assert next_generation == [
-        Cell(CellState.DEAD, 0),
-        Cell(CellState.ALIVE, 1),
-        Cell(CellState.ALIVE, 2),
-        Cell(CellState.DEAD, 3),
+        Cell(CellState.DEAD, (0, 0)),
+        Cell(CellState.ALIVE, (1, 0)),
+        Cell(CellState.ALIVE, (2, 0)),
+        Cell(CellState.DEAD, (3, 0)),
     ]
