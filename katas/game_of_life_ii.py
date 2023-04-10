ab624ffe28cebca366af71d969667f2d1df7fe37
polyccon <polyccon@gmail.com>
2023-04-10 12:14:31 +0100

    R move neighbour calculation to cell class

--- katas/game_of_life_ii.py
+++ katas/game_of_life_ii.py
@@ -37,22 +37,22 @@ class Cell:
             CellState.DEAD if number_of_neighbours < 2 else CellState.ALIVE
         )
 
+    def calculate_number_of_neighbours(self, board_cells):
+        count = 0
+        neighbour_positions = self.get_neighbour_positions()
+        for neighbour_position in neighbour_positions:
+            if neighbour_position in [
+                (cell.position.x, cell.position.y) for cell in board_cells
+            ]:
+                count += 1
+        return count
+
 
 class Game:
     def __init__(self, board) -> None:
         self.board = board
 
-    def calculate_number_of_neighbours(self, cell):
-        count = 0
-        neighbour_positions = cell.get_neighbour_positions()
-        for neighbour_position in neighbour_positions:
-            if neighbour_position in [
-                (cell.position.x, cell.position.y) for cell in self.board
-            ]:
-                count += 1
-        return count
-
     def play(self):
         for cell in self.board:
-            number_of_neighbours = self.calculate_number_of_neighbours(cell)
+            number_of_neighbours = cell.calculate_number_of_neighbours(self.board)
             cell.evolve(number_of_neighbours)
